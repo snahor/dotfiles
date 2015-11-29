39fee41823409863d65abd70861818d9e402f872
Hans Roman <hans.r.69@gmail.com>
2015-11-28 22:59:10 -0500

    add symlinks for neovim

--- install.py
+++ install.py
@@ -8,6 +8,16 @@ from glob import glob
 dotfiles = path.dirname(path.abspath(__file__))
 home = path.dirname(dotfiles)
 
+nvim_dir = path.expanduser('~/.config/nvim')
+vim_dir = path.expanduser('~/.vim')
+nvimrc = path.expanduser('~/.config/nvim/init.vim')
+vimrc = path.expanduser('~/.vimrc')
+
+
+def _symlink(target, link):
+    print('Symlinking: %s -> %s' % (link, target))
+    os.symlink(target, link)
+
 
 def symlink(target):
     if path.isdir(target):
@@ -19,11 +29,15 @@ def symlink(target):
             dirname = path.dirname(link)
             if not path.exists(dirname):
                 os.makedirs(dirname)
-            print('Symlinking: %s -> %s' % (link, target))
-            os.symlink(target, link)
+            _symlink(target, link)
 
 
 for target in glob(dotfiles + '/.*'):
     if target.endswith('.git'):
         continue
     symlink(target)
+
+
+if not path.islink(nvim_dir) and not path.exists(nvim_dir):
+    _symlink(vim_dir, nvim_dir)
+    _symlink(vimrc, nvimrc)
